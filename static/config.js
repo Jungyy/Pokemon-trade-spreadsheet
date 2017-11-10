d11b1fdca1245aa5c70d313c11e35a454944bd2e
Jungy <33554001+Jungyy@users.noreply.github.com>
2017-11-10 12:52:25 -0500

    Add files via upload

---
+++ static/config.js
@@ -0,0 +1,22 @@
+﻿// ***********************************************************************************
+// ID of the Google spreadsheet
+// ***********************************************************************************
+// Make sure your spreadsheet is published (File > Publish to the web…)
+var spreadsheetId = "1WQHj1U2MF3ozjm7q40tJsc-roeyFwtPuuLAEI5Vv9Qo";
+
+// ***********************************************************************************
+// Your FC and IGN
+// ***********************************************************************************
+var friendCode = "2724-1366-3884";
+var inGameName = "Castiel";
+
+// ***********************************************************************************
+// URL of your Reddit/Twitter account
+// ***********************************************************************************
+var contactUrl = "http://reddit.com/u/Jungyy";
+
+// ***********************************************************************************
+// URL of your PGL trainer icon (optional)
+// ***********************************************************************************
+// Download it from the Pokémon Global Link and re-upload to an image-sharing website
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
